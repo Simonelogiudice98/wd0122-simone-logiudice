81a006ee6cef32bc4882bf8dc51e183e7f2484cc
Simonelogiudice98 <logiusimone@gmail.com>
2022-05-17 15:53:22 +0200

    bw

--- m2/w4/bw/js/script.js
+++ m2/w4/bw/js/script.js
@@ -1,5 +1,35 @@
+
 let tbody = document.querySelector('#utenti')
 let thead = document.querySelector('#thead')
+let inputId = document.querySelector('#inputId')
+let nome1 = document.querySelector('#name1')
+let userName1 = document.querySelector('#userName1')
+let email1 = document.querySelector('#mail1')
+let invia1 = document.querySelector('#invia1')
+let nomeV = document.querySelector('#nomeV')
+let indirizzo = document.querySelector('#indirizzo')
+let azienda = document.querySelector('#azienda')
+let telefono = document.querySelector('#telefono')
+
+
+
+        // invia1.addEventListener('click',function(e){
+        //     e.preventDefault()
+            
+
+        //     fetch("http://jsonplaceholder.typicode.com/users" + "/" + inputId.value, {
+        //     method: 'PATCH',
+        //     body: JSON.stringify({
+        //         name : nome1.value,
+        //         username : userName1.value,
+        //         email : email1.value
+        //     }),
+        //     headers: { 'Content-type': 'Application/json'}
+        //  }).then(function(response){
+        //     console.log(response);
+        //     })
+
+        // })
 
 fetch("http://jsonplaceholder.typicode.com/users")
 .then(res => res.json())
@@ -26,35 +56,68 @@ fetch("http://jsonplaceholder.typicode.com/users")
         let vedi = document.createElement('button')
         vedi.classList.add('btn','bg-info')
         vedi.innerHTML = 'View'
+        let vediUtente = document.querySelector('.vediUtente')
+
+        vedi.addEventListener('click',function(){
+
+            vediUtente.classList.toggle('visibile')
+            nomeV.innerHTML = utente.name
+            document.getElementById("azienda").innerHTML = JSON.stringify(utente.company.name);
+            document.getElementById("indirizzo").innerHTML = JSON.stringify(utente.address.street +' '+ utente.address.suite + ' ' + utente.address.city);
+            telefono.innerHTML = utente.phone
+
+        })
 
         let modifica = document.createElement('button')
         modifica.classList.add('btn','bg-warning','my-2')
+        modifica.setAttribute('data-bs-toggle','modal')
+        modifica.setAttribute('data-bs-target','#exampleModal2')
+        console.log(modifica.getAttribute('data-bs-toggle'));
         modifica.innerHTML = 'Edit'
-        modifica.addEventListener('click', function(){
-            fetch("http://jsonplaceholder.typicode.com/users/", {
-            method: 'PATCH',
-            body: JSON.stringify(utente),
-            headers: { 'Content-type': 'Application/json'}
-         }).then(function(response){
-            console.log(response);
-            })
+        let modificaUtente = document.querySelector('.modificaUtente')
+        
+        modifica.addEventListener('click',function(){
+            
+            // modificaUtente.classList.toggle('visibile')
+            
+            nome1.value = utente.name
+            userName1.value = utente.username
+            email1.value = utente.email
+            inputId.value = utente.id
+            
         })
+
 
         let elimina = document.createElement('button')
         elimina.classList.add('btn','bg-danger','text-white',)
         elimina.innerHTML = 'Delete'
+
         elimina.addEventListener('click',function(){
-            fetch('http://jsonplaceholder.typicode.com/users/'+'/'+ utente.id, {
-            method: 'DELETE',
+            let counts = setInterval(updated,20);
+            let upto = 0
+            let width = 0
+            function updated(){
+                const count = document.getElementById('counter')
+                const hr = document.getElementById('hr')
+                ++upto;
+                count.innerHTML = 'LOADING ' + upto + '%';
+                hr.style.width = upto +'%'
+                if(upto === 100){
+                    clearInterval(counts);
+                    count.innerHTML = 'DONE';
+                }}
+            fetch("http://jsonplaceholder.typicode.com/users"+'/'+ utente.id,{
+                method: 'DELETE'
             })
-            .then(res =>{
-                console.log(res);
-                if(res.status == 200 || res.status == 201){
-                    window.alert('Utente eliminato con successo!')
-                }
-            })
-        
+            setTimeout(function() {Swal.fire({
+                title: 'Error!',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+              })},2000)
         })
+
+
 
         let rowButton = document.createElement('div')
         rowButton.classList.add('row')
@@ -65,50 +128,59 @@ fetch("http://jsonplaceholder.typicode.com/users")
 
         tbody.append(tr)
 
-        console.log(utente);
     }
-    
 })
+// let toggleUtente = document.querySelector('.aggiungiUtente')
+// let aggiungi = document.querySelector('#aggiungi');
+// aggiungi.addEventListener('click',function(){
 
+//     toggleUtente.classList.toggle('visibile')
 
-
-let divForm = document.querySelector('.aggiungiUtente')
-let aggiungi = document.querySelector('#aggiungi')
-aggiungi.addEventListener('click', function(){
-
-    divForm.classList.toggle('visibile')
-})
-
-
+// })
+var divCounter = document.querySelector('.div-counter')
 let nome = document.querySelector('#name')
 let userName = document.querySelector('#userName')
-let mail = document.querySelector('#mail')
+let email = document.querySelector('#mail')
 let invia = document.querySelector('#invia')
 
-invia.addEventListener('click', function(e){
+invia.addEventListener('click',function(e){
     e.preventDefault()
-    let user = {
-        name:  nome.value,
+
+    let counts = setInterval(updated,20);
+    let upto = 0
+    let width = 0
+    function updated(){
+        const count = document.getElementById('counter')
+        const hr = document.getElementById('hr')
+        ++upto;
+        count.innerHTML = 'LOADING ' + upto + '%';
+        hr.style.width = upto +'%'
+        if(upto === 100){
+            clearInterval(counts);
+            count.innerHTML = 'DONE';
+            
+        }
+        divCounter.append(counter,hr)
+    }
+    let users = {
+        name: nome.value,
         username: userName.value,
         email: mail.value,
     }
     fetch("http://jsonplaceholder.typicode.com/users/", {
         method: 'POST',
-        body: JSON.stringify(user),
+        body: JSON.stringify(users),
         headers: { 'Content-type': 'Application/json'}
     })
-    
     .then(res =>{
         console.log(res);
         if(res.status == 200 || res.status == 201){
-            window.alert('Utente aggiunto con successo!')
+            setTimeout(function() {Swal.fire({
+                title: 'successo!',
+                text: 'Utente aggiunto',
+                icon: 'success',
+                confirmButtonText: 'ok'
+              })},2000)
         }
     })
-
 })
-
-
-
-
-// .then(res => res.text()) 
-// .then(res => console.log(res))
